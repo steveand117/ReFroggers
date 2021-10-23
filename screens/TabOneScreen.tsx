023a1f1850054f5ed54dc86e534db99f34e4e155
Alice Zeng <azeng30@gatech.edu>
2021-10-23 16:27:28 -0400

    update home screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,39 +1,98 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, Alert, TextInput, Pressable, Button } from 'react-native';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const scanPressed = ()=> {
+    navigation.navigate('Home');
+  }
+  const frogPressed = ()=> {
+    navigation.navigate('Home');
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Froggers</Text>
-      <Text style={styles.title}>Scan a QR code to get started</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.bruh}>Bruh</Text>
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Welcome Back!</Text>
+      <Image
+            source={require('../assets/images/wizardfrog.png')}
+            style={styles.frog}
+            resizeMode="contain"
+      >
+      </Image>
+      <Text style={styles.rando}>your ecofrog:</Text>
+      <Text style={styles.rando}>your wizard frog:</Text>
+
+      <TouchableOpacity onPress={scanPressed} style={styles.button}>
+            <Text style= {styles.buttonText}>scan something!</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={frogPressed} style={styles.button}>
+            <Text style= {styles.buttonText}>choose companion</Text>
+      </TouchableOpacity>
+
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      >
+      </Image>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E3FCAE",
   },
-  title: {
-    fontSize: 20,
+  title:{
+    color: '#42A840',
+    width: "100%",
+    textAlign: 'center',
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    marginLeft: '2%',
+    fontSize:  20,
+    fontFamily: 'press-start',
+    marginTop: '15%',
   },
   bruh: {
     fontSize: 40,
     fontWeight: '900'
+  },
+  frog: {
+    width: 200,
+    height: 200,
+    marginTop: '5%'
+  },
+  rando: {
+   color: '#42A840',
+   borderRadius: 25,
+   textAlign: 'center',
+   fontWeight: 'bold',
+   marginRight: '2%',
+   fontSize:  12,
+   fontFamily: 'press-start'
+  },
+  button: {
+    backgroundColor: '#42A840',
+    width: "70%",
+    borderRadius: 10,
+    padding: 13,
+    fontSize:  27,
+    marginTop: '4%',
+    fontFamily: 'press-start',
+  },
+  buttonText: {
+    color: 'white',
+    marginLeft: '15%',
+    fontSize:  12,
+    fontFamily: 'press-start',
+  },
+  logo:{
+    width: 100,
+    height: 100,
+    marginTop: '5%'
   },
 });
