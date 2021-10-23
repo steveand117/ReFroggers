efc965f47da1a0e55ce29b2e05d9644c171c9af3
Alice Zeng <azeng30@gatech.edu>
2021-10-23 00:56:10 -0400

    qr scanner

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,6 +10,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.bruh}>Bruh</Text>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -29,5 +30,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  bruh: {
+    fontSize: 40,
+    fontWeight: '900'
   },
 });
--- types.tsx
+++ types.tsx
@@ -26,7 +26,8 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 
 export type RootTabParamList = {
   TabOne: undefined;
-  TabTwo: undefined;
+  TabTwo: {data: string};
+  TabThree: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
